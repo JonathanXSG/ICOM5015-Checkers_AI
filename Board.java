39a6b38299b39e8b5e6c3288ed2adad4b7be3913
EduardoPerez13 <eduardo.perez13@upr.edu>
2019-05-02 00:45:28 -0400

    Added condition to prevent making a diagonal move while there is a jump available.

--- Board.java
+++ Board.java
@@ -100,28 +100,32 @@ public class Board {
 	        return false;
         else if(board(finalCord) != ' ')
             return false;
-        else if(!getValidDiagonals(initialCord.posX, initialCord.posY, player).contains(finalCord) && !getValidJumps(initialCord.posX, initialCord.posY, player, king,true).contains(finalCord)) {
+        else if(!getValidDiagonals(initialCord.posX, initialCord.posY, player).contains(finalCord) && !getValidJumps(initialCord.posX, initialCord.posY, player, king,true).contains(finalCord))
             return false;
-        }
-        else if(!king && player == Piece.Black && finalCord.posY < initialCord.posY){
+        else if(getValidDiagonals(initialCord.posX, initialCord.posY, player).contains(finalCord) && jumpsAvailable(player))
             return false;
-        }
-        else if(!king && player == Piece.Red && finalCord.posY > initialCord.posY){
-            return false;
-        }
         else{
             if(getValidJumps(initialCord.posX, initialCord.posY, player, king, true).contains(finalCord)){
                 //Jump over opponent
                 board(new Pair<>((finalCord.posX+initialCord.posX)/2,(finalCord.posY+initialCord.posY)/2), ' ');
             }
+            board(finalCord, board(initialCord));
             if(finalCord.posY == size-1 || finalCord.posY == 0)
-                board(finalCord, Character.toUpperCase(board(initialCord)));
-            else
-                board(finalCord, board(initialCord));
+                makeKing(finalCord.posX, finalCord.posY);
+                //board(finalCord, Character.toUpperCase(board(initialCord)));
             board(initialCord, ' ');
             return true;
         }
     }
+
+    public boolean jumpsAvailable(Piece player){
+	    for (Pair<Integer, Integer> piece: getAllPieceLocations(player)){
+	        if(!getValidJumps(piece.posX, piece.posY, player, isPieceKing(piece.posX, piece.posY), true).isEmpty())
+	            return true;
+        }
+	    return false;
+    }
+
 
     public int[][] calcValues(Piece player){
 	    int[][] values = new int[size][size];
@@ -211,7 +215,7 @@ public class Board {
     	            diagonals.add(new Pair<>(x+modifier, y+modifier));
     		 if(!isInvalidCord(x-modifier, y+modifier) && boardState[x-modifier][y+modifier] == ' ')
     	            diagonals.add(new Pair<>(x-modifier, y+modifier));
-    	}else if(boardState[x][y] != ' ' && (boardState[x][y] == (player == Piece.Black? 'b' : 'r'))){
+    	}else if(boardState[x][y] != ' ' && (boardState[x][y] == (player == Piece.Black? 'B' : 'R'))){
     		if(!isInvalidCord(x+modifier, y+modifier)  && boardState[x+modifier][y+modifier] == ' ')
                 diagonals.add(new Pair<>(x+modifier, y+modifier));
             if(!isInvalidCord(x+modifier, y-modifier)  && boardState[x+modifier][y-modifier] == ' ')
